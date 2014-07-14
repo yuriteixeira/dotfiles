417db7936258a190d48c1d63d6e96b0ae9dbebc1
Yuri Teixeira <oyuriteixeira@gmail.com>
2014-07-14 22:08:13 +0200

    Fix problems on linux machine

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@
 find . -name ".*" -type f | grep -v ".gitignore" | awk -F './' '{print $2}' | xargs -I file ln -sf $(pwd)/file ~/
 
 ### Oh-my-zsh customizations
-find ./oh-my-zsh-custom/plugins -mindepth 1 -maxdepth 1 -type d | xargs -I file ln -sf $(pwd)/file ~/.oh-my-zsh/custom/plugins
+find ./oh-my-zsh-custom/plugins -mindepth 1 -maxdepth 1 -type d | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/plugins
 
 echo "Dotfile Installed!"
