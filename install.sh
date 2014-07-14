e0b73c9cdb9689440f19991276092c5b0817ebf7
Yuri Teixeira <oyuriteixeira@gmail.com>
2014-07-14 22:14:04 +0200

    Fix symlinks generation for dotfiles

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### All dotfiles on home dir
-find . -name ".*" -type f | grep -v ".gitignore" | grep -v ".swp" | awk -F './' '{print $2}' | xargs -I file ln -sf $(pwd)/file ~/
+find . -name ".*" -type f | grep -v ".gitignore" | grep -v ".swp" | awk -F './' '{print $2}' | xargs -I {} ln -sf $(pwd)/{} ~/
 
 ### Oh-my-zsh customizations
 find ./oh-my-zsh-custom/plugins -mindepth 1 -maxdepth 1 -type d | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/plugins
