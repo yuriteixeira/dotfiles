c2732d523af863deeef1bcfbd5709c9ccad524ab
Yuri Teixeira <oyuriteixeira@gmail.com>
2014-07-31 11:51:17 +0200

    Installer: themes support

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 find . -name ".*" -type f | grep -v ".gitignore" | grep -v ".swp" | awk -F './' '{print $2}' | xargs -I {} ln -sf $(pwd)/{} ~/
 
 ### Oh-my-zsh customizations
+mkdir -p ~/.oh-my-zsh/custom/themes
 find ./oh-my-zsh-custom/plugins -mindepth 1 -maxdepth 1 -type d | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/plugins
+find ./oh-my-zsh-custom/themes -mindepth 1 -maxdepth 1 -type f | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/themes
 
-echo "Dotfile Installed!"
+echo "Dotfiles Installed!"
