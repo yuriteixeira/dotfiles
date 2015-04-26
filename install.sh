018517604a80aa21a3db77424e8d8160596005b7
Yuri Teixeira <oyuriteixeira@gmail.com>
2015-04-26 20:19:41 +0200

    Improves install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ### All dotfiles on home dir
-find . -name ".*" | grep -v ".git" | grep -v ".swp" | awk -F './' '{print $2}' | xargs -I {} ln -sf $(pwd)/{} ~/
+find . -type f -name ".*" | grep -v ".gitignore" | grep -v ".DS_Store" | grep -v ".swp" | awk -F './' '{print $2}' | xargs -I {} ln -sf $(pwd)/{} ~/
 
 ### Oh-my-zsh customizations
 mkdir -p ~/.oh-my-zsh/custom/themes
+mkdir -p ~/.oh-my-zsh/custom/plugins
 find ./oh-my-zsh-custom/plugins -mindepth 1 -maxdepth 1 -type d | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/plugins
 find ./oh-my-zsh-custom/themes -mindepth 1 -maxdepth 1 -type f | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/themes
 
