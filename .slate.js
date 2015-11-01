a7ee665a6980f29a2b261ca6caa82c5a9e2ef9ef
Yuri Teixeira <oyuriteixeira@gmail.com>
2015-11-01 15:01:31 +0100

    [Slate] Small improvements
    * Hyper + Key bindings (Snapshots and App Focus)

--- .slate.js
+++ .slate.js
@@ -1,7 +1,7 @@
 slate.configAll({
   defaultToCurrentScreen: true,
   resizePercentOf: 'screenSize',
-  windowHintsIgnoreHiddenWindows: true,
+  windowHintsIgnoreHiddenWindows: false,
   windowHintsSpread: false,
   windowHintsShowIcons: false,
   windowHintsDuration: 3,
@@ -24,8 +24,12 @@ slate.bind('2:' + hyperKey, slate.operation('throw', {screen: 'right', height: '
 slate.bind('r:' + hyperKey, slate.operation('relaunch'));
 slate.bind('g:' + hyperKey, slate.operation('grid'));
 slate.bind('u:' + hyperKey, slate.operation('undo'));
+slate.bind('0:' + hyperKey, slate.operation('snapshot', {name: 'default', save: true}));
+slate.bind('/:' + hyperKey, slate.operation('activate-snapshot', {name: 'default'}));
 
 slate.bind('esc:' + hyperKey, slate.operation('hint'));
+slate.bind('h:' + hyperKey, slate.operation('focus', {direction: 'left'}));
+slate.bind('l:' + hyperKey, slate.operation('focus', {direction: 'right'}));
 slate.bind('left:' + hyperKey, slate.operation('focus', {direction: 'left'}));
 slate.bind('right:' + hyperKey, slate.operation('focus', {direction: 'right'}));
 
@@ -34,3 +38,8 @@ slate.bind('j:' + hyperKey, slate.operation('focus', {app: 'IntelliJ IDEA'}));
 slate.bind('c:' + hyperKey, slate.operation('focus', {app: 'Google Chrome'}));
 slate.bind('i:' + hyperKey, slate.operation('focus', {app: 'iTerm'}));
 slate.bind('s:' + hyperKey, slate.operation('focus', {app: 'Slack'}));
+slate.bind('m:' + hyperKey, slate.operation('focus', {app: 'Spotify'}));
+
+slate.bind('f11', slate.operation('focus', {app: 'Google Chrome'}));
+slate.bind('f12', slate.operation('focus', {app: 'iTerm'}));
+slate.bind('f13', slate.operation('focus', {app: 'Atom'}));
