326ac43d7fe5eea3c9f82c293c04675233ef68c6
Yuri Teixeira <oyuriteixeira@gmail.com>
2017-03-13 17:27:01 +0100

    [DOCKER] Better aliases

--- oh-my-zsh-custom/plugins/common-aliases/common-aliases.plugin.zsh
+++ oh-my-zsh-custom/plugins/common-aliases/common-aliases.plugin.zsh
@@ -79,11 +79,8 @@ alias bcs="brew cask search"
 alias d=docker
 alias di='docker images'
 alias dc="docker-compose"
-
-function dcleanup {
-    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
-}
+alias dk='docker system prune -f'
+alias ddf='docker system df'
 
 # Go
 alias mygo="export GOPATH=$MY_GOPATH"
