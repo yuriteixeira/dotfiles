b25417703fa97b0aeafc23e8bff19c5d2bf07574
Yuri Teixeira <oyuriteixeira@gmail.com>
2017-03-13 17:35:38 +0100

    [INSTALL] Check if folders exist before do stuff

--- install.sh
+++ install.sh
@@ -13,10 +13,14 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 ### Oh-my-zsh customizations
-mkdir -p ~/.oh-my-zsh/custom/themes
-mkdir -p ~/.oh-my-zsh/custom/plugins
+if [ -d ./oh-my-zsh-custom/plugins ]; then
+    mkdir -p ~/.oh-my-zsh/custom/plugins
+    find ./oh-my-zsh-custom/plugins -mindepth 1 -maxdepth 1 -type d | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/plugins
+fi
 
-find ./oh-my-zsh-custom/plugins -mindepth 1 -maxdepth 1 -type d | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/plugins
-find ./oh-my-zsh-custom/themes -mindepth 1 -maxdepth 1 -type f | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/themes
+if [ -d ./oh-my-zsh-custom/themes ]; then
+    mkdir -p ~/.oh-my-zsh/custom/themes
+    find ./oh-my-zsh-custom/themes -mindepth 1 -maxdepth 1 -type f | xargs -I {} ln -sf $(pwd)/{} ~/.oh-my-zsh/custom/themes
+fi
 
 echo "Dotfiles Installed!"
